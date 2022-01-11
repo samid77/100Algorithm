63184f18b932cb540893060822f3e227000b7dbb
Samid77 <dimasseptyantop@gmail.com>
2022-01-11 09:39:11 +0700

    More exercise

--- avoidObstacles/avoidObstacles.ts
+++ avoidObstacles/avoidObstacles.ts
@@ -1,5 +1,16 @@
-export function avoidObstacles(inputArray: number[]): number {
+export function avoidObstacles(inputArray: number[]) {
+
+  inputArray = inputArray.sort((a,b) => a - b);
+  const largestVal = inputArray[inputArray.length - 1];
+
+  for (let i = 1; i < largestVal + 1; i++) {
+    if(inputArray.every((el) => el % i !== 0)) {
+      return i;
+    }
+    console.log(inputArray.every((el) => el % i !== 0))
+  }
+
 
 }
 
-// console.log(avoidObstacles([5, 3, 6, 7, 9]));
+console.log(avoidObstacles([5, 3, 6, 7, 9]));
--- boxBlur/boxBlur.ts
+++ boxBlur/boxBlur.ts
@@ -2,6 +2,6 @@ export function boxBlur(image: number[][]): number[][] {
 
 }
 
-// console.log(boxBlur([[1, 1, 1], 
-//     [1, 7, 1], 
-//     [1, 1, 1]]));
+console.log(boxBlur([[1, 1, 1], 
+    [1, 7, 1], 
+    [1, 1, 1]]));
