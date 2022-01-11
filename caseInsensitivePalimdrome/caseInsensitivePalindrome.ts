02036073b418353523e0da4c0f27d4d360ab044e
Samid77 <dimasseptyantop@gmail.com>
2022-01-11 09:05:45 +0700

    More exercise

--- caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
+++ caseInsensitivePalimdrome/caseInsensitivePalindrome.ts
@@ -1,6 +1,11 @@
 export function isCaseInsensitivePalindrome(inputString: string): boolean {
 
+  const originalLowerCase = inputString.toLowerCase();
+  const reversedWord = inputString.toLowerCase().split('').reverse().join('');
+
+  return originalLowerCase === reversedWord;
+
 }
 
-// console.log(isCaseInsensitivePalindrome('AaBaa'));
-// console.log(isCaseInsensitivePalindrome('abac'));
+console.log(isCaseInsensitivePalindrome('AaBaa'));
+console.log(isCaseInsensitivePalindrome('abac'));
