f386e2f7097236f8bf441bfbdda871e53308dfd5
samid77 <gn.dimas@gmail.com>
2022-04-05 11:47:31 +0700

    Working on companyBotStrategy and compareIntegers

--- compareIntegers/compareIntegers.ts
+++ compareIntegers/compareIntegers.ts
@@ -1,7 +1,8 @@
 export function compareIntegers(a: string, b: string): string {
 
+    return parseInt(a) > parseInt(b) ? 'greater' : parseInt(a) < parseInt(b) ? 'less' : 'equal'
 }
 
-// console.log(compareIntegers('12', '13'));
-// console.log(compareIntegers('875', '799'));
-// console.log(compareIntegers('1000', '1000'));
+console.log(compareIntegers('12', '13'));
+console.log(compareIntegers('875', '799'));
+console.log(compareIntegers('1000', '1000'));
