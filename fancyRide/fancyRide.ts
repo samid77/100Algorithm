c85d8ee607603f0f3d866f34a32a0b2bc85d689e
ds77 <Dimas@Inass-Mac-Air.local>
2022-04-11 15:59:37 +0700

    Solve some

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,13 @@
 export function fancyRide(l: number, fares: number[]): string {
 
+    const rides: string[] = ['UberX', 'UberXL', 'UberPlus', 'UberBlack', 'UberSUV']
+    for(let i=fares.length - 1; i >= 0; i--) {
+        if(fares[i] * l <= 20) {
+            return rides[i]
+        }
+    }
+
+    return '';
 }
 
-// console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
