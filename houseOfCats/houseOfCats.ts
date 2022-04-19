1f21ec59683341e2dc42e36b2f61787eba151e50
Samid77 <dimasseptyantop@gmail.com>
2022-04-19 08:40:13 +0700

    Solve: houseNumbersSum, houseOfCat, incorrectPasswordAttempt and htmlEndTag

--- houseOfCats/houseOfCats.ts
+++ houseOfCats/houseOfCats.ts
@@ -1,6 +1,17 @@
 export function houseOfCats(legs: number): number[] {
+    let people: number[] = [];
 
+    if(legs === 2) {
+        return [1];
+    }
+
+    while(legs >= 0) {
+        people.unshift(legs/2)
+        legs -= 4
+    }
+
+    return people
 }
 
-// console.log(houseOfCats(6));
-// console.log(houseOfCats(2));
+console.log(houseOfCats(6));
+console.log(houseOfCats(2));
--- htmlEndTagByStartTag/htmlEndTagByStartTag.ts
+++ htmlEndTagByStartTag/htmlEndTagByStartTag.ts
@@ -1,6 +1,17 @@
 export function htmlEndTagByStartTag(startTag: string): string {
 
+     const splitStr = startTag.split(' ');
+     const splitTag = splitStr[0].toString().split('');
+     let endTag = '</';
+
+     for(let i = 1; i < splitTag.length; i++) {
+         endTag += splitTag[i]
+     }
+
+     endTag = endTag[endTag.length - 1] === '>' ? endTag : endTag += '>';
+
+     return endTag
 }
 
-// console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
-// console.log(htmlEndTagByStartTag('<i>'));
+console.log(htmlEndTagByStartTag("<button type='button' disabled>"));
+console.log(htmlEndTagByStartTag('<i>'));
