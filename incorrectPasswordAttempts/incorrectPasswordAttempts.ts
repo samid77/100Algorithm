02db998d32056e75ea12cce5c7672ac00893f4f6
Samid77 <dimasseptyantop@gmail.com>
2022-04-19 08:39:06 +0700

    Solve: houseNumbersSum, houseOfCat, incorrectPasswordAttempt and htmlEndTag

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,17 @@
 export function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
 
+    let failedAttempt = 0;
+
+    for(let attempt of attempts) {
+        failedAttempt = attempt === passcode ? 0 : failedAttempt++;
+        if(failedAttempt === 10) {
+            return true;
+        }
+    }
+
+    return false;
+
 }
 
-// console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "6666", "1111"]));
