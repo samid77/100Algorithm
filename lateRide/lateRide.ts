620ee125adc34699f13889e107383de7f1674adc
Samid77 <dimasseptyantop@gmail.com>
2022-04-22 16:53:40 +0700

    Solve largestNumber, largestofFour and lateRide

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,6 +1,15 @@
 export function lateRide(n: number): number {
+    const hours = Math.floor(n / 60);
+    const minutes = n % 60;
+    const time = hours.toString().concat(minutes.toString()).split('').map((char) => (
+        parseInt(char)
+    ));
+
+    console.log(time)
+
+    return time.reduce((a,b) => a + b)
 
 }
 
-// console.log(lateRide(240));
-// console.log(lateRide(808));
+console.log(lateRide(240));
+console.log(lateRide(808));
