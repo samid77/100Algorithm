bcbe3271541355a9e9f906a739fdadf08fce45c2
Samid77 <dimasseptyantop@gmail.com>
2022-04-22 16:54:03 +0700

    Solved another

--- largestNumber/largestNumber.ts
+++ largestNumber/largestNumber.ts
@@ -1,5 +1,16 @@
 export function largestNumber(n: number): number {
 
+    let word: string = ''
+
+    // for(let i=0; i < n; i++){
+    //     word = word.concat('9');
+    // }
+
+    // return parseInt(word);
+
+    word = '9'
+    return parseInt(word.repeat(n));
 }
 
-// console.log(largestNumber(2));
+console.log(largestNumber(2));
+console.log(largestNumber(4));
--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,6 +1,22 @@
 export function largestOfFour(nums: number[][]): number[] {
 
+    let result: number[] = []
+
+    for(let numberGroup of nums) {
+        let largest = 0;
+        console.log(`numberGroup: ${numberGroup}`)
+
+        for(let number of numberGroup) {
+            console.log(`number: ${number}`)
+            largest = largest < number ? number : largest;
+        }
+
+        result.push(largest)
+    }
+
+    return result;
+
 }
 
-// console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
-// console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
+console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
