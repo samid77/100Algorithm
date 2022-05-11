e31fea70388e811848c4f1e822734d130353a32c
Samid77 <dimasseptyantop@gmail.com>
2022-05-11 08:28:49 +0700

    Solve some

--- sortByHeight/sortByHeight.ts
+++ sortByHeight/sortByHeight.ts
@@ -1,5 +1,17 @@
 export function sortByHeight(a: number[]): number[] {
+    const sortedArr = a.filter(e => {
+        if(e !== -1) return e;
+    }).sort((a,b) => a-b)
 
+    let adjustedIdx = 0;
+    for(let i=0; i < a.length; i++) {
+        if(a[i] !== -1) {
+            a[i] = sortedArr[adjustedIdx]
+            adjustedIdx++;
+        }
+    }
+
+    return a;
 }
 
-// console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
+console.log(sortByHeight([-1, 150, 190, 170, -1, -1, 160, 180]));
