3c3d72d69b73dd67437d93791828eb6c89eb53d8
Samid77 <dimasseptyantop@gmail.com>
2022-05-19 14:21:35 +0700

    Solve uniqueDigitProducts, sumOddFibonacci

--- uniqueDigitProducts/uniqueDigitsProducts.ts
+++ uniqueDigitProducts/uniqueDigitsProducts.ts
@@ -1,4 +1,22 @@
 export function uniqueDigitProducts(a: number[]): number {
+    const distinctProduct: number[] = [];
+    a.forEach((digit) => {
+        const totalDigit = getTotalDigit(digit);
+        console.log(`totalDigit: ${totalDigit}`)
+        if(!distinctProduct.includes(totalDigit)) distinctProduct.push(totalDigit)
+    })
+
+    return distinctProduct.length
 }
 
-// console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
+const getTotalDigit = (num: number) => {
+    const arrDigits: string[] = num.toString().split('');
+    let total = 1;
+    arrDigits.forEach(d => {
+        total *= parseInt(d);
+    })
+
+    return total;
+}
+
+console.log(uniqueDigitProducts([2, 8, 121, 42, 222, 23]));
