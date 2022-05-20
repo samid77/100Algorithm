623648369d8c0f5f63643a6b02cfe2275e0697a5
Samid77 <dimasseptyantop@gmail.com>
2022-05-20 07:07:44 +0700

    Solve validTime

--- validTime/validTime.ts
+++ validTime/validTime.ts
@@ -1,7 +1,12 @@
 export function validTime(time: string): boolean {
+    const [hours, minutes] = time.split(':');
+    
+    if(parseInt(hours) < 0 || parseInt(hours) > 23) return false;
+    if(parseInt(minutes) > 59 || parseInt(minutes) < 0) return false;
 
+    return true;
 }
 
-// console.log(validTime('13:58'));
-// console.log(validTime('25:51'));
-// console.log(validTime('02:76'));
+console.log(validTime('13:58'));
+console.log(validTime('25:51'));
+console.log(validTime('02:76'));
