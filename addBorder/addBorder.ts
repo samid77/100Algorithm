e8969fd6d975f56d65fc254943f8e05577e0a946
Samid77 <dimasseptyantop@gmail.com>
2022-05-24 07:58:30 +0700

    Solve addBorder

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,32 @@
-export function addBorder(picture: string[]): string[] {
+export function addBorder(picture: string[]): any {
+    const wallLength: number = picture[0].length + 2;
+    let wall: string = '';
 
+    for(let i=0; i < wallLength; i++) {
+        wall = wall.concat('*');
+    }
+
+    picture.unshift(wall);
+    picture.push(wall);
+
+    for(let i=1; i < picture.length - 1; i++) {
+        picture[i] = '*'.concat(picture[i] + '*')
+    }
+    
+    return picture;
+}
+export function addBorderAlternative(picture: string[]): any {
+    const wall: string = '*'.repeat(picture[0].length + 2)
+
+    picture.unshift(wall);
+    picture.push(wall);
+
+    for(let i=1; i < picture.length - 1; i++) {
+        picture[i] = '*'.concat(picture[i] + '*');
+    }
+    
+    return picture;
 }
 
-// console.log(addBorder(["abc", "ded"]));
+console.log(addBorder(["abc", "ded"]));
+console.log(addBorderAlternative(["abc", "ded"]));
