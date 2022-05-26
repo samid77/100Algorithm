65d6a8acad9befd8c0510150c3bd3bc4cf0518d7
Samid77 <dimasseptyantop@gmail.com>
2022-05-26 11:08:47 +0700

    Solve allLongestString

--- allLongestStrings/allLongestStrings.ts
+++ allLongestStrings/allLongestStrings.ts
@@ -1,4 +1,16 @@
 export function allLongestStrings(inputArray: string[]): string[] {
+    let longestLength: number = 0;
+    let longestWords:string[] = []
+
+    inputArray.forEach((word:string) => {
+        longestLength = longestLength < word.length ? word.length : longestLength;
+    });
+
+    inputArray.forEach((word: string) => {
+        if(word.length === longestLength) longestWords.push(word)
+    });
+    
+    return longestWords;
 
 }
 
