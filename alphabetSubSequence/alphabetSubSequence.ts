1ed970010b4a31d1e761bd356494d54ca391a3f1
Samid77 <dimasseptyantop@gmail.com>
2022-05-30 09:07:30 +0700

    Solve alphabetSubSequence

--- alphabetSubSequence/alphabetSubSequence.ts
+++ alphabetSubSequence/alphabetSubSequence.ts
@@ -1,5 +1,23 @@
 export function alphabetSubsequence(s: string): boolean {
 
+    const chars: string[] = s.split('');
+    const charsValue: number[] = [];
+
+    chars.forEach((c: string) => {
+        charsValue.push(c.charCodeAt(0))
+    })
+
+    console.log(charsValue);
+    console.log(new Set(charsValue))
+    console.log(new Set(charsValue).size)
+
+    if(new Set(charsValue).size !== charsValue.length) return false;
+
+    for(let i=0; i < charsValue.length - 1; i++) {
+        if(charsValue[i] >= charsValue[i + 1]) return false;
+    }
+
+    return true;
 }
 
 console.log(alphabetSubsequence('zab'))
