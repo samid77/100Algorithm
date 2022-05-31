8956a1e36cfd21f695aee53d1edde8130834658e
Samid77 <dimasseptyantop@gmail.com>
2022-05-31 09:08:26 +0700

    Solve alternatingSum

--- alternatingSums/alternatingSums.ts
+++ alternatingSums/alternatingSums.ts
@@ -1,5 +1,16 @@
 export function alternatingSums(a: number[]): number[] {
+    let evenSum: number = 0;
+    let oddSum: number = 0;
 
+    a.forEach((el, idx) => {
+        if(idx % 2 === 0) {
+            evenSum += el
+        } else {
+            oddSum += el;
+        }
+    })
+
+    return [evenSum, oddSum]
 }
 
 console.log(alternatingSums([50, 60, 60, 45, 70]))
